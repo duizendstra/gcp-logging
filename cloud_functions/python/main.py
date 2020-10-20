01718822f3577138aa713b37b5821d19783f4da9
Jasper Duizendstra <jasper@olympus.local>
2020-10-20 18:42:02 +0200

    test

--- cloud_functions/python/main.py
+++ cloud_functions/python/main.py
@@ -1,4 +1,7 @@
+import logging
 from flask import Response
 
 def cloud_function_python(request):
+    logging.warning('Watch out!')  # will print a message to the console
+    logging.info('I told you so')  # will not print anything
     return Response("", status=201)
