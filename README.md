ff23e1eba4276833d02109ce361796a8b209d0fd
Jasper Duizendstra <jasper@olympus.local>
2020-10-20 21:56:12 +0200

    test

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Logging best practices on GCP per language and environment
 
 
 https://docs.python.org/3/howto/logging.html
+https://cloud.google.com/service-infrastructure/docs/service-control/reference/rpc/google.logging.type
--- cloud_functions/python/main.py
+++ cloud_functions/python/main.py
@@ -11,10 +11,64 @@ def cloud_function_python(request):
 
     logging.debug("logging.debug")
     logging.info("logging.info")
-
     logging.warning("logging.warning") 
     logging.error("logging.error")
     logging.exception("logging.exception") 
     logging.critical("logging.critical") 
- 
+
+
+    entry = dict(severity='DEFAULT',
+             message='The log entry has no assigned severity level.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='DEBUG',
+             message='Debug or trace information.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='INFO',
+             message='Routine information, such as ongoing status or performance.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='NOTICE',
+             message='Normal but significant events, such as start up, shut down, or a configuration change.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='WARNING',
+             message='Warning events might cause problems.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='EMERGENCY',
+             message='Error events are likely to cause problems.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='CRITICAL',
+             message='Critical events cause more severe problems or outages.',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry))
+    entry = dict(severity='ALERT',
+             message='A person must take an action immediately',
+             component='arbitrary-property',
+             **global_log_fields)
+
+    print(json.dumps(entry)) 
+    entry = dict(severity='EMERGENCY',
+             message='One or more systems are unusable.',
+             component='arbitrary-property',
+             **global_log_fields)
+     print(json.dumps(entry))
+
     return Response("cloud_function_python", status=200)
